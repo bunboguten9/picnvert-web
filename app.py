e0fa9ba40aefdf1303337cd0625a8de5abe19e41
bunboguten9 <bunbogutenhasubara9@gmail.com>
2025-07-14 12:54:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def download_file(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
